9f450d08d67b126012b2d2d6ec0e516fea558bfd
tnaka <t-nakashima@jp.ricoh.com>
2025-10-29 17:20:32 +0900

    openaiへのアクセス追加

--- src/LeftArea.js
+++ src/LeftArea.js
@@ -2,12 +2,39 @@
 import React, {useState} from 'react';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import axios from 'axios';
+
 
 const LeftArea = () => {
     const [prompt, setPrompt] = useState('');
+    const [response, setResponse] = useState('');
+        
+    const createPost = async () => {
+        try {
+            const response = await axios.post(
+                // 環境変数AZURE_OPENAI_ENDPOINTを使う(後ろに/openai/v1/responsesをつける)
+                process.env.REACT_APP_AZURE_OPENAI_ENDPOINT + 'openai/v1/responses',
+                {
+                    model: 'gpt-4.1-2025-04-14',
+                    input: prompt,
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'api-key': process.env.REACT_APP_AZURE_OPENAI_API_KEY,
+                    },
+                }
+            );
+            setResponse(response.data.output[0].content[0].text);
+        } catch (error) {
+            console.error('Error creating post:', error);
+            setResponse('エラーが発生しました: ' + error.message);
+        }
+    }
+
     return (
-        <Stack sx={{ width: '100%', height: '100%', padding: '20px'}}>
+        <Stack sx={{ width: '100%', height: '100%', padding: '20px'}} spacing={2}>
         <TextField 
             sx={{ width: '90%' }} 
             id="prompt" 
@@ -18,10 +45,23 @@ const LeftArea = () => {
             onChange={(e) => setPrompt(e.target.value)}
         />
 
-        <Typography variant="body1" sx={{ whiteSpace: 'pre-wrap' }}>
-            入力されたプロンプト: {prompt}
-        </Typography>
-        
+        <Button variant="contained" sx={{width: '90%'}} onClick={createPost}> AI 実行!</Button>
+        <TextField 
+            sx={{ width: '90%' }} 
+            id="output" 
+            label="生成物" 
+            placeholder='生成物がここに表示されます' 
+            variant="outlined" 
+            multiline rows={20}
+            value={response}
+
+            slotProps={{
+                inputLabel: {
+                    shrink: true, // ラベルを常に縮小表示
+                }
+            }}
+        />
+
         </Stack>
     );
 };
