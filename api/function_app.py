45a9a2516cc4fb1f6293c54736b5882bdf56cdc5
tnaka <t-nakashima@jp.ricoh.com>
2025-10-31 09:01:22 +0900

    API(python)にチャレンジ

--- api/function_app.py
+++ api/function_app.py
@@ -1,5 +1,6 @@
 import azure.functions as func
 import logging
+import json
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -17,9 +18,10 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(json.dumps({"data":f"Hello, {name}. This HTTP triggered function executed successfully."}), mimetype="application/json", status_code=200)
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             json.dumps({"data":"This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."}),
+             mimetype="application/json",
              status_code=200
         )
